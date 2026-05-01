8e035514499b1bb5c3b3bad3be317baa66ada9d9
Allen Chen <allenchen@Allens-MacBook-Air.local>
2026-04-30 20:35:12 -0400

    added homography to setup

--- final_challenge/setup.py
+++ final_challenge/setup.py
@@ -34,6 +34,7 @@ setup(
             'traffic_light_controller = final_challenge.traffic_light_controller:main',
             'traffic_light_color_detector = final_challenge.traffic_light_color_detector:main',
             'yolo_final_challenge = final_challenge.yolo_final_challenge:main',
+            'homography_transform = final_challenge.homography_transform:main',
         ],
     },
 )
