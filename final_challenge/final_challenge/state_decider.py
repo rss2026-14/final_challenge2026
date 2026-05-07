5a16193cf1d1825c26aee06c2e1be3e38893f56d
crystalmelon <cheniseoharper@gmail.com>
2026-05-07 02:04:14 +0000

    no more float(speed)

--- final_challenge/final_challenge/state_decider.py
+++ final_challenge/final_challenge/state_decider.py
@@ -296,7 +296,7 @@ class BoatingExecutive(Node):
         drive_cmd = AckermannDriveStamped()
         drive_cmd.header.stamp = self.get_clock().now().to_msg()
         drive_cmd.header.frame_id = "base_link"
-        drive_cmd.drive.speed = float(speed)
+        drive_cmd.drive.speed = speed
         drive_cmd.drive.steering_angle = float(steering_angle)
 
         self.drive_pub.publish(drive_cmd)
