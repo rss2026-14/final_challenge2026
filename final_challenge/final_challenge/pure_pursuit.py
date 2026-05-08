4996053ffc7777aac4f7f1a74d39591044fd7d1e
Hoives <pwmason@mit.edu>
2026-05-07 22:17:15 -0400

    idk

--- final_challenge/final_challenge/pure_pursuit.py
+++ final_challenge/final_challenge/pure_pursuit.py
@@ -94,9 +94,7 @@ class PurePursuit(Node):
         actual_lookahead_sq = target_x**2 + target_y**2
         steering_angle = np.arctan2(2*self.wheelbase_length*target_y, actual_lookahead_sq)
 
-        if steering_angle < 0.15:
-            steering_angle *= 0.25
-        else:
+        if steering_angle > 0.15:
             steering_angle *= 0.55
         steering_angle=np.clip(steering_angle,-0.34,0.34)
         speed=self.base_speed-6.0*abs(steering_angle)
