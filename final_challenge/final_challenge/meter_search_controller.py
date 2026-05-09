e77b302e072009900fd9c42fa969b4e36997b6f1
crystalmelon <cheniseoharper@gmail.com>
2026-05-09 09:01:44 +0000

    duration

--- final_challenge/final_challenge/meter_search_controller.py
+++ final_challenge/final_challenge/meter_search_controller.py
@@ -55,7 +55,7 @@ class MeterSearchController(Node):
         self.declare_parameter("forward_search_angle", math.pi / 6.0)
 
         self.declare_parameter("k_turn_forward_duration", 0.35)
-        self.declare_parameter("k_turn_reverse_duration", 0.45)
+        self.declare_parameter("k_turn_reverse_duration", 0.7)
 
         self.declare_parameter("reverse_pause_duration", 0.25)
 
