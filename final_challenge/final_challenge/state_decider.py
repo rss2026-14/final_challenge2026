a653385016e8182a96c333ab0a808d37abe51ea7
crystalmelon <cheniseoharper@gmail.com>
2026-05-09 11:08:16 +0000

    4 secos

--- final_challenge/final_challenge/state_decider.py
+++ final_challenge/final_challenge/state_decider.py
@@ -490,7 +490,7 @@ class BoatingExecutive(Node):
                 self.get_clock().now() - self.reverse_start_time
             ).nanoseconds * 1e-9
 
-            if elapsed > 2.0:
+            if elapsed > 4.0:
                 self.get_logger().info("Reverse complete. Resuming navigation.")
                 self.hit_the_brakes()
                 self.set_state(State.NAVIGATING)
