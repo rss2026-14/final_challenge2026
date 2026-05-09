be82b5c78ef0fc47c818d69b694f0de7022160f6
crystalmelon <cheniseoharper@gmail.com>
2026-05-09 09:13:59 +0000

    ms change

--- final_challenge/final_challenge/meter_search_controller.py
+++ final_challenge/final_challenge/meter_search_controller.py
@@ -59,7 +59,7 @@ class MeterSearchController(Node):
 
         self.declare_parameter("reverse_pause_duration", 0.25)
 
-        self.declare_parameter("max_k_turn_cycles", 12)
+        self.declare_parameter("max_k_turn_cycles", 15)
 
         self.scan_topic = self.get_parameter("scan_topic").value
         self.drive_topic = self.get_parameter("drive_topic").value
